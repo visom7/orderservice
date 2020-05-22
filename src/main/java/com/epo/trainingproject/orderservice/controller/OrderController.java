7cc7389fd2be3d81fcc50a9d935f1acaeb565c7c
visom7 <Isabel977*>
2020-05-22 09:21:46 +0200

    Added comunication with shipping

--- src/main/java/com/epo/trainingproject/orderservice/controller/OrderController.java
+++ src/main/java/com/epo/trainingproject/orderservice/controller/OrderController.java
@@ -4,8 +4,10 @@ import com.epo.trainingproject.orderservice.model.ProductOrderModel;
 import com.epo.trainingproject.orderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,9 +24,4 @@ public class OrderController {
         orderService.makeOrder(productOrderModels);
     }
 
-    @GetMapping("/ship")
-    public ResponseEntity shippingMock() {
-        log.info("Send product");
-        return ResponseEntity.ok().build();
-    }
 }
--- src/main/java/com/epo/trainingproject/orderservice/service/implementation/OrderServiceImpl.java
+++ src/main/java/com/epo/trainingproject/orderservice/service/implementation/OrderServiceImpl.java
@@ -5,8 +5,11 @@ import com.epo.trainingproject.orderservice.service.OrderService;
 import com.epo.trainingproject.orderservice.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -24,15 +27,20 @@ public class OrderServiceImpl implements OrderService {
             if (productService.checkAvailability(productOrderModel.getProductId()) < productOrderModel.getQuantity()) {
                 productService.addStock(productOrderModel.getProductId(), productOrderModel.getQuantity());
             }
-            getHttpCall();
+            httpPostProductOrderModelsTo("http://localhost:8091","shipping/ship", productOrderModels);
             productService.decreaseStock(productOrderModel.getProductId(), productOrderModel.getQuantity());
         });
     }
 
-    private void getHttpCall() {
-        WebClient client = WebClient.builder().baseUrl("http://localhost:8090").build();
-        WebClient.RequestBodySpec requestBodySpec = client.method(HttpMethod.GET).uri("/order/ship");
-        String response = requestBodySpec.exchange().block().bodyToMono(String.class).block();
-        log.info(response);
+    private void httpPostProductOrderModelsTo(String baseUrl, String endpoint, List<ProductOrderModel> productOrderModels) {
+        WebClient client = WebClient.builder().baseUrl(baseUrl).build();
+        WebClient.RequestHeadersSpec<?> requestHeadersSpec = client
+                .method(HttpMethod.POST)
+                .uri(endpoint)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(BodyInserters.fromValue(productOrderModels));
+        log.info("#### Sending request to " + baseUrl + endpoint);
+        String response = requestHeadersSpec.exchange().block().bodyToMono(String.class).block();
+        log.info("#### Received response: " + response);
     }
 }
